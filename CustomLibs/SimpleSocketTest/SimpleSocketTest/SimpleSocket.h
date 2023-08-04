bcccda3ad5642abbfd57f590db2d07868fbd7381
ACD <psvitaday2@gmail.com>
2023-08-04 17:38:10 +0300

    Initiated custom socket library

---
+++ CustomLibs/SimpleSocketTest/SimpleSocketTest/SimpleSocket.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <WinSock2.h>
+#include <iostream>
+#include <string>
+#include<map>
+
+namespace SS{
+
+	class SimpleSocket
+	{
+	private:
+		std::string node_name;
+		bool type;
+
+
+	public:
+		SimpleSocket();
+	};
+
+}
+
